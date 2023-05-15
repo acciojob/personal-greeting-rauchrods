ea8fb279c6c1bf48bb9827d1ef7719459da36765
rauch <rauchrodrigues@gmail.com>
2023-05-15 16:54:45 +0530

    1st push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [name,setname]=useState("");
+
+  function changefunc(e){
+     setname("Hello " + e.target.value + "!");
+  }
+
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div>       
+        <label htmlFor="name">Enter your name:</label>
+        <input type="text" name="" id="name" onChange={changefunc}/>
+        <p>{name}</p>
     </div>
   )
 }
